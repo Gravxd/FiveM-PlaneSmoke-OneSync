de962e66cc5661e6f1fd9f9be3ce97e3059ffedb
Grav <75702884+Gravxd@users.noreply.github.com>
2021-01-20 13:02:06 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'JayMontana36'
+description 'Remade Plane Smoke Script For OneSync - upgraded by Grav#6969'
+version '1.1-JM36_BETA-1.2'
+
+client_script 'client.lua'
+server_script 'server.lua'
